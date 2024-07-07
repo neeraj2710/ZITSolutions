7763f745cd711016950f9987003f377683a6422c
neera <neerajwadhwaney2003@gmail.com>
2024-07-07 14:29:01 +0530

    07-07-2024

---
+++ src/in/co/zitsolutions/task2_multithreading/using_lambda_expression/Main.java
@@ -0,0 +1,22 @@
+package in.co.zitsolutions.task2_multithreading.using_lambda_expression;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread t1 =  new Thread(()->{
+           for(int i = 1 ; i <= 5 ; i++){
+               System.out.println("Hi");
+               try{Thread.sleep(1000);}catch(InterruptedException ignored){}
+           }
+        });
+        Thread t2 = new Thread(()->{
+            for(int i = 1 ; i <= 5 ; i++){
+                System.out.println("Hello");
+                try{Thread.sleep(1000);}catch(InterruptedException ignored){}
+            }
+        });
+
+        t1.start();
+        try{Thread.sleep(10);}catch(InterruptedException ignored){}
+        t2.start();
+    }
+}
